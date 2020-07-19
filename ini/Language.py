c4a8a84879721f5ce1d615ea1588af1e24bc9133
hjm8377 <hjm8377@gmail.com>
2020-07-19 16:20:07 +0900

    for language setting

---
+++ ini/Language.py
@@ -0,0 +1,106 @@
+Ok = {
+    'English': 'OK',
+    'Korean': '확인'
+}
+
+Cancel = {
+    'English': 'Cancel',
+    'Korean': '취소'
+}
+
+File = {
+    'English': 'File',
+    'Korean': '파일'
+}
+
+Help = {
+    'English': 'Help',
+    'Korean': '도움'
+}
+
+Exit = {
+    'English': 'Exit',
+    'Korean': '종료'
+}
+
+Previous = {
+    'English': 'Prev',
+    'Korean': '이전'
+}
+
+Next = {
+    'English': 'Next',
+    'Korean': '다음'
+}
+
+Setting = {
+    'English': 'Setting',
+    'Korean': '설정'
+}
+
+How = {
+    'English': 'How',
+    'Korean': '사용방법'
+}
+
+Info = {
+    'English': 'Info',
+    'Korean': '정보'
+}
+
+Rotation_Speed = {
+    'English': 'Rotation Speed',
+    'Korean': '회전 속도'
+}
+
+Language = {
+    'English': 'Language',
+    'Korean': '언어'
+}
+
+Solving = {
+    'English': 'Solving',
+    'Korean': '솔빙'
+}
+
+Complete = {
+    'English': 'Complete',
+    'Korean': '완료'
+}
+
+Set = {
+    'English': 'Set',
+    'Korean': '설정'
+}
+
+Warning_ = {
+    'English': 'Warning',
+    'Korean': '경고'
+}
+
+lang_warning = {
+    'English': '언어 변경을 완전히 적용하려면 프로그램을 재시작 해야합니다.',
+    'Korean': 'You must restart the program to apply the language change.'
+}
+
+howText = {
+    'English': '1. Scramble a cube\n\n 2. Press solving button \n\n '
+               '3. Recognize all faces of the cube to the cam \nand press complete button \n',
+    'Korean': '1. 큐브를 섞는다\n\n 2. 솔빙 버튼을 누른다\n\n '
+              '3. 캠에 큐브의 모든 면을 인식시킨다 \n'
+}
+
+sText = {
+    'English': 'with Beginner\'s method and CFOP method',
+    'Korean': '초급 공식과 CFOP 공식 사용'
+}
+
+Homepage = {
+    'English': 'Homepage',
+    'Korean': '홈페이지'
+}
+
+Made = {
+    'English': 'made by J.M. Heo',
+    'Korean': '제작: 허정민'
+}
